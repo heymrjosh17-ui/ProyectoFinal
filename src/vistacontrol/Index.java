d62d3d27f9a4f98f45fb9ab1a4ffb542ffdef644
heyjhonna <heyjhonna@HeyJhonna>
2026-06-23 00:27:51 -0500

    Primer commit Segundos cambios finales Cambios cambios

--- src/vistacontrol/Index.java
+++ src/vistacontrol/Index.java
@@ -10,49 +10,7 @@ import utils.Utilitarios;
 public class Index {
 
     private static final Lectura leer = new Lectura();
-
-    public static boolean loginmostrar() {
-
-        String usuariocorrecto = "usiel";
-        String clavecorrecta = "123";
-
-        System.out.println("/// Usuario /// ");
-        String usuario = leer.cadena();
-
-        System.out.println("/// Contraseña /// ");
-        String clave = leer.cadena();
-
-        if (usuario.equals(usuariocorrecto) && clave.equals(clavecorrecta)) {
-            System.out.println("Bienvenido al sistema");
-            return true;
-        } else {
-            System.out.println("Usuario o contraseña incorrectos");
-        }
-        return false;
-    }
-
-    public static void login() {
-        int intentos = 0;
-        int limitedeint = 3;
-        while (intentos < limitedeint) {
-            if (loginmostrar()) {
-                inicio();
-                return;
-            } else {
-                intentos++;
-                int restantes = limitedeint - intentos;
-                if (intentos == 1) {
-                    System.out.println("Te queda " + restantes + " intentos");
-                } else {
-                    System.out.println("Te quedan " + restantes + " intento");
-                }
-                if (intentos == 3) {
-                    System.out.println("Usuario bloqueado");
-                }
-            }
-        }
-    }
-
+   
     public static void cositas() {
         Utilitarios.limpiarPantalla();
         Utilitarios.imprimirSeparador();
@@ -98,6 +56,6 @@ public class Index {
     }
 
     public static void main(String[] args) {
-        login();
+        IndexLogin.login();
     }
 }
