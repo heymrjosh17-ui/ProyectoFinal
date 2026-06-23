4010d415a3e3b6bcec782a4ed191897dc4ef9da0
heyjhonna <heyjhonna@HeyJhonna>
2026-06-23 00:28:05 -0500

    Primer commit Segundos cambios finales Cambios cambios camnios

---
+++ src/vistacontrol/IndexLogin.java
@@ -0,0 +1,53 @@
+package vistacontrol;
+
+import utils.Lectura;
+
+/**
+ *
+ * @author heyjhonna
+ */
+public class IndexLogin {
+
+    public static Lectura leer = new Lectura();
+
+    public static boolean loginmostrar() {
+        String usuariocorrecto = "usiel";
+        String clavecorrecta = "123";
+
+        System.out.println("/// Usuario /// ");
+        String usuario = leer.cadena();
+
+        System.out.println("/// Contraseña /// ");
+        String clave = leer.cadena();
+
+        if (usuario.equals(usuariocorrecto) && clave.equals(clavecorrecta)) {
+            System.out.println("Bienvenido al sistema");
+            return true;
+        } else {
+            System.out.println("Usuario o contraseña incorrectos");
+        }
+        return false;
+    }
+
+    public static void login() {
+        int intentos = 0;
+        int limitedeint = 3;
+        while (intentos < limitedeint) {
+            if (loginmostrar()) {
+                Index.inicio();
+                return;
+            } else {
+                intentos++;
+                int restantes = limitedeint - intentos;
+                if (intentos == 1) {
+                    System.out.println("Te queda " + restantes + " intentos");
+                } else {
+                    System.out.println("Te quedan " + restantes + " intento");
+                }
+                if (intentos == 3) {
+                    System.out.println("Usuario bloqueado");
+                }
+            }
+        }
+    }
+}
